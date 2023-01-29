99f930847aa597f922fb3d8d9fa90eb7c193d47c
Alessandrror <116864696+Alessandrror@users.noreply.github.com>
2023-01-29 13:09:55 -0600

    Update app.js

---
+++ interactive-rating-component-main/app.js
@@ -0,0 +1,13 @@
+const submit = document.querySelector('#rate-btn');
+const r1 = document.querySelector('#s1');
+const r2 = document.querySelector('#s2');
+const r3 = document.querySelector('#s3');
+const r4 = document.querySelector('#s4');
+const r5 = document.querySelector('#s5');
+const rating = document.querySelector('.selection')
+
+submit.addEventListener('click',rated);
+
+function rated() {
+    return rated; 
+}
