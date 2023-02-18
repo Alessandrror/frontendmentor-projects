3bc6aa80b4ed896f6e934bc5f799681a8e2fb2ff
Alessandro <ssandrorivas@gmail.com>
2023-02-17 18:32:00 -0600

    adding the JS script link to html document

---
+++ faq-accordion-card-main/app.js
@@ -0,0 +1,5 @@
+let active = 0;
+let innactive = 1;
+var togbtn = document.getElementsByClassName('toggle-btn');
+
+// let btn = addEventListener('click', (togbtn) => document.getElementsByClassName('faq-answer').classList.toggle('appear'));
